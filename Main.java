6c5a0c97f31dff81f35b26a79f05950792e4f323
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-04-06 19:02:24 -0400

    GUI

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+
+        // Creates an instance of the constructor and essentially duplicates the code inside the constructor.
+        MyFrame welcome = new MyFrame();
+        LaunchSeasonalIngredientsPage launchPage01 = new LaunchSeasonalIngredientsPage();
     }
 }

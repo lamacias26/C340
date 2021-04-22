30a8fb28bb0f2329985e370cfd4cd51172de6600
darlynmendez <77795140+darlynmendez@users.noreply.github.com>
2021-04-22 17:48:31 -0400

    Changing scenes

--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
-import javax.swing.*;
-import java.awt.*;
-import javax.swing.JLabel;
-import javax.swing.border.Border;
 
+package gui;
 public class Main {
-    public static void main(String[] args) {
 
-        // Creates an instance of the constructor and essentially duplicates the code inside the constructor.
-        MyFrame welcome = new MyFrame();
-        LaunchSeasonalIngredientsPage launchPage01 = new LaunchSeasonalIngredientsPage();
+    public static void main(String[] args) {
+        RTIUI.Gui();
     }
 }
